d372e8326cd6c6624a3d95c296605532b228c0bc
poushali <poushalibanik03@gmail.com>
2024-01-01 16:26:21 +0530

    day23 q solved

---
+++ Day23/day23.js
@@ -0,0 +1,12 @@
+// In this coding challenge, you will be implementing a function called getValues(object) that 
+// takes an argument: an object. The function should return an array will all of the object’s 
+// values.
+
+// Instructions:-
+// If the object has no values, return an empty array.
+
+//Solution:-
+
+export const getValues = (object) => {
+	return Object.values(object);
+}
